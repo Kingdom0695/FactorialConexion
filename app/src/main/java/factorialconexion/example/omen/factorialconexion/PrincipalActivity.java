d187431102d4eb1d070e0446155cd560ebeb393f
Kingdom0695 <kingdom0695@gmail.com>
2018-06-07 11:50:12 -0500

    Segundo commit: se agrego el permiso de internet y se hizo la prueba con la conexion de la aplicacion.

--- app/src/main/java/factorialconexion/example/omen/factorialconexion/PrincipalActivity.java
+++ app/src/main/java/factorialconexion/example/omen/factorialconexion/PrincipalActivity.java
@@ -2,9 +2,21 @@ package factorialconexion.example.omen.factorialconexion;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class PrincipalActivity extends AppCompatActivity {
 
@@ -14,7 +26,42 @@ public class PrincipalActivity extends AppCompatActivity {
         setContentView(R.layout.activity_principal);
 
         Button btn = (Button) findViewById(R.id.btn1);
-        EditText edt = (EditText) findViewById(R.id.edt1);
-        TextView txtview = (TextView) findViewById(R.id.txtview1);
+        final EditText edt = (EditText) findViewById(R.id.edt1);
+        final TextView txtview = (TextView) findViewById(R.id.txtview1);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String url = "http://192.168.84.45/ejemplojson/imprimejson.php?numero=" + edt.getText().toString();
+                JsonObjectRequest peticion = new JsonObjectRequest
+                        (
+                                Request.Method.GET,
+                                url,
+                                null,
+                                new Response.Listener<JSONObject>()
+                                {
+                                    @Override
+                                    public void onResponse(JSONObject response)
+                                    {
+                                        try {
+                                            txtview.setText("" + response.getInt("factorial"));
+
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                }
+                                , new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(PrincipalActivity.this,error.getMessage(),Toast.LENGTH_LONG).show();
+                            }
+                        });
+                RequestQueue x = Volley.newRequestQueue(PrincipalActivity.this);
+                x.add(peticion);
+
+            }
+        });
     }
 }
